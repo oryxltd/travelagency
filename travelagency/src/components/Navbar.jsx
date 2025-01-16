7af3ada5f877772a21b1036644b824712d467e6b
devanshtiwari2002 <tiwaridevanshtech1@gmail.com>
2025-01-16 23:53:24 +0530

    feat(navbar) : Implement responsive naviagtion with support of  - add responsive navbar compoent with mobile-first design - implemnt hamburger menu toggle for mobile view - Add whatsapp redirect funtionality - - add smooth transition for menu toggle

--- travelagency/src/components/Navbar.jsx
+++ travelagency/src/components/Navbar.jsx
@@ -1,18 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+import tempImg from "../assets/images/tempImg.jpg";
+import "../components/navbar.css";
 
 export default function Navbar() {
+  const [click, setClick] = React.useState(false);
+
+  const handleClick = () => setClick(!click);
+
   return (
-    <div>
-      <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/destination">Destination</Link></li>
-        <li><Link to="/gallery">Gallery</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-      </ul>
-    </nav>
+    <div className="navbar">
+      <div className="logo">
+        <img src={tempImg} alt="logoImg" />
+        <h5>JK Tours</h5>
+      </div>
+      <div className={`nav-links ${click ? "active" : ""}`}>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/destination">Destination</Link>
+            </li>
+            <li>
+              <Link to="/gallery">Gallery</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="hamburger" onClick={handleClick}>
+        {click ? <FaTimes /> : <FaBars />}
+      </div>
+      <div className="whatsapp">
+        <a href="https://wa.me/+917581867478" target="_blank" rel="noreferrer">
+          whatsapp
+        </a>
+      </div>
     </div>
-  )
+  );
 }
